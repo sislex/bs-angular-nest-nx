1ec89325f44cebb465b6a5b87fb5535adf6873f7
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-08-12 16:25:51 +0300

    session

--- apps/back-app/src/main.ts
+++ apps/back-app/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4200',
+    origin: 'h*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
