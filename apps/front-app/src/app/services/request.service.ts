2a5cc92104c7d140982c71d2d475deb7426a371e
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-08-19 13:08:34 +0300

    -  changed '' for `` in request.service.ts

--- apps/front-app/src/app/services/request.service.ts
+++ apps/front-app/src/app/services/request.service.ts
@@ -2,18 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class RequestService {
-  private baseUrl = 'http://185.244.50.198:3000';
+  private baseUrl = `http://185.244.50.198:3000`;
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   sendRequest(requestData: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/requests`, requestData, { withCredentials: true });
   }
-
-
 }
