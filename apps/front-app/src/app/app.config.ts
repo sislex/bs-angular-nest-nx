60346a38290edb04625187cb46bfe98692d4c336
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-08-19 15:50:19 +0300

    - delete withFetch() -add phpAdmin

--- apps/front-app/src/app/app.config.ts
+++ apps/front-app/src/app/app.config.ts
@@ -2,11 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
